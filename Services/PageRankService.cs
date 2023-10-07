dbf431928c5ce4669bd178e0f26013fed2b53894
em1rR <emirhanaydinat1@gmail.com>
2023-10-07 19:46:40 +0300

    PageRankService get method update

--- Services/PageRankService.cs
+++ Services/PageRankService.cs
@@ -17,48 +17,16 @@ namespace AlgorithmsWebAPI.Services
         //    _context = context;
         //}
         
-
-       
-
         public void pageRank()
         {
-            //// A most valuable
-            //Dictionary<string, List<string>> dicIncoming = new Dictionary<string, List<string>>()
-            //{
-            //    { "A", new List<string>{"B", "D" } },
-            //    { "B", new List<string>{"A", "C", "D" } },
-            //    { "C", new List<string>{"A" } },
-            //    { "D", new List<string>{"A", "C" } },
-            //};
-            //Dictionary<string, List<string>> dicOutgoing = new Dictionary<string, List<string>>()
-            //{
-            //    { "A", new List<string>{"A", "B", "C", "D"} },
-            //    { "B", new List<string>{"A" } },
-            //    { "C", new List<string>{"B", "D"} },
-            //    { "D", new List<string>{"A", "B" } },
-            //};
-            // D most valuable
-            Dictionary<string, List<string>> dicIncoming = new Dictionary<string, List<string>>()
-            {
-                { "A", new List<string>{"B" } },
-                { "B", new List<string>{"A" } },
-                { "C", new List<string>{"A" } },
-                { "D", new List<string>{"A", "C", "B" } },
-            };
-            Dictionary<string, List<string>> dicOutgoing = new Dictionary<string, List<string>>()
-            {
-                { "A", new List<string>{"A", "B", "C", "D"} },
-                { "B", new List<string>{"D" } },
-                { "C", new List<string>{"B", "D"} },
-                { "D", new List<string>{"A", "B" } },
-            };
-            Dictionary<string, double> ranks = new Dictionary<string, double>()
-            {
-                { "A", 0.25 },
-                { "B", 0.25 },
-                { "C", 0.25 },
-                { "D", 0.25 },
-            };
+            var data = new List<Dictionary<string, List<string>>>();
+            var ranks = new Dictionary<string, double>();
+
+            data = GetData("A");
+            var dicIncoming = data[0];
+            var dicOutgoing = data[1];
+            ranks = GetRank();
+
             double dampingFactor = 0.85;
             int iteration = 20;
 
@@ -80,6 +48,61 @@ namespace AlgorithmsWebAPI.Services
             };
         }
 
+        private List<Dictionary<string, List<string>>> GetData(string mostValuable)
+        {
+            List<Dictionary<string, List<string>>> data = new List<Dictionary<string,List<string>>>();
+            if (mostValuable == "A" || mostValuable == "a")
+            {
+                Dictionary<string, List<string>> dicIncoming = new Dictionary<string, List<string>>()
+                {
+                    { "A", new List<string>{"B", "D" } },
+                    { "B", new List<string>{"A", "C", "D" } },
+                    { "C", new List<string>{"A" } },
+                    { "D", new List<string>{"A", "C" } },
+                };
+                Dictionary<string, List<string>> dicOutgoing = new Dictionary<string, List<string>>()
+                {
+                    { "A", new List<string>{"A", "B", "C", "D"} },
+                    { "B", new List<string>{"A" } },
+                    { "C", new List<string>{"B", "D"} },
+                    { "D", new List<string>{"A", "B" } },
+                };
+                data.Add(dicIncoming);
+                data.Add(dicOutgoing);
+            }
+            else if (mostValuable == "D" || mostValuable == "d")
+            {
+                Dictionary<string, List<string>> dicIncoming = new Dictionary<string, List<string>>()
+                {
+                    { "A", new List<string>{"B" } },
+                    { "B", new List<string>{"A" } },
+                    { "C", new List<string>{"A" } },
+                    { "D", new List<string>{"A", "C", "B" } },
+                };
+                Dictionary<string, List<string>> dicOutgoing = new Dictionary<string, List<string>>()
+                {
+                    { "A", new List<string>{"A", "B", "C", "D"} },
+                    { "B", new List<string>{"D" } },
+                    { "C", new List<string>{"B", "D"} },
+                    { "D", new List<string>{"A", "B" } },
+                };
+                data.Add(dicIncoming);
+                data.Add(dicOutgoing);
+            }
+            return data;
+        }
+
+        private Dictionary<string, double> GetRank()
+        {
+            Dictionary<string, double> ranks = new Dictionary<string, double>()
+            {
+                { "A", 0.25 },
+                { "B", 0.25 },
+                { "C", 0.25 },
+                { "D", 0.25 },
+            };
+            return ranks;
+        }
 
         //public async Task<IEnumerable<Invoice>> GetInvoiceList()
         //{
